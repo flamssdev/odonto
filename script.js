2807fa59387d2424895201ee9a41402c99ea0916
Flávio Emanuel <109769529+flamssdev@users.noreply.github.com>
2023-03-03 11:00:00 -0300

    inicial

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 var palavras = ["Tenha a melhor ", "Tenha a melhor ", "Tenha ", "Permita-se "];
 var complemento = ["AUTOESTIMA", "CONFIANÇA", "AMOR-PRÓPRIO", "SORRIR MAIS"];
 var indicePalavra = 0;
+var intervalo;
+
+// function atualizarPalavra() {
+//   document.getElementById("palavras").innerHTML = palavras[indicePalavra];
+//   document.getElementById("palavra-teste").innerHTML = complemento[indicePalavra];
+//   indicePalavra++;
+//   if (indicePalavra >= palavras.length) {
+//     indicePalavra = palavras.length - 1;
+//   }
+// }
+
+
 
 function atualizarPalavra() {
-  document.getElementById("palavras").innerHTML = palavras[indicePalavra];
-  document.getElementById("palavra-teste").innerHTML = complemento[indicePalavra];
-  indicePalavra++;
-  if (indicePalavra >= palavras.length) {
-    indicePalavra = palavras.length - 1;
-  }
+  var elementoPalavra = document.getElementById("palavras");
+  let elementoComplemento = document.getElementById("palavra-teste");
+  elementoPalavra.style.opacity = 0;
+  elementoComplemento.style.opacity = 0
+  setTimeout(function() {
+    elementoPalavra.innerHTML = palavras[indicePalavra];
+    elementoPalavra.style.opacity = 1;
+    elementoComplemento.innerHTML = complemento[indicePalavra];
+    elementoComplemento.style.opacity = 1;
+    if (indicePalavra >= palavras.length - 1) {
+      clearInterval(intervalo);
+    }
+    indicePalavra++;
+  }, 1000);
 }
 
-setInterval(atualizarPalavra, 3000);
+
+intervalo = setInterval(atualizarPalavra, 3000);
