161ebedce1f1bd6a62f88b4f2cc91668b370ecba
Flávio Emanuel <109769529+flamssdev@users.noreply.github.com>
2023-03-03 10:41:59 -0300

    inicial

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var palavras = ["Tenha a melhor AUTOESTIMA", "Tenha a melhor CONFIANÇA", "Tenha AMOR-PRÓPRIO", "Permita-se SORRIR MAIS"];
+var palavras = ["Tenha a melhor ", "Tenha a melhor ", "Tenha ", "Permita-se "];
+var complemento = ["AUTOESTIMA", "CONFIANÇA", "AMOR-PRÓPRIO", "SORRIR MAIS"];
 var indicePalavra = 0;
 
 function atualizarPalavra() {
   document.getElementById("palavras").innerHTML = palavras[indicePalavra];
+  document.getElementById("palavra-teste").innerHTML = complemento[indicePalavra];
   indicePalavra++;
   if (indicePalavra >= palavras.length) {
     indicePalavra = palavras.length - 1;
